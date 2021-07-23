f164f417c59acbf1e19a949a8b2b23315cabe66c
elaneealne8531@gmail.com <elaneelane8531@gmail.com>
2021-07-23 13:08:37 +0800

    Update 2021-07-23T05:08:25.566Z

---
+++ assets/js/all.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+"use strict";
+
+console.log('qqq');
+//# sourceMappingURL=all.js.map
